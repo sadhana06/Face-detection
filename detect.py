dfdd2a434f8af740f66f0054b3894d4cbe87b0bd
sadhana06 <61183702+sadhana06@users.noreply.github.com>
2023-03-09 15:48:42 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,9 +4,9 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # To capture video from webcam.
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 # To use a video file as input
-cap = cv2.VideoCapture('filename.mp4')
+#cap = cv2.VideoCapture('filename.mp4')
 
 while True:
     # Read the frame
